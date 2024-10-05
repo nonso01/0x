eb2a6327a2766392764f68c928b49820d2de1a1b
nonso01 <chukwunonsomartin01@gmail.com>
2024-10-06 00:52:44 +0100

    meta-data

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,17 @@ import "./globals.css";
 export const metadata = {
   title: "Nonso Martin",
   description: "Creative Web Developer | 3D artist | Rust Evangelist",
+  generator: "Next.js",
+  applicationName: "nonso01.vercel.app",
+  referrer: "origin-when-cross-origin",
+  keywords: ["Javascript", "Blender", "Theeejs"],
+  authors: [{ name: "Nonso" }],
+  creator: "Nonso",
+  publisher: "Nonso Martin",
+};
+
+export const viewport = {
+  themeColor: "#222222",
 };
 
 export default function RootLayout({ children }) {
